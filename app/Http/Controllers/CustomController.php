51ae7cfc1a08451bed145b958b0222b0a8b0bbce
kashifmushtaq114@gmail.com <kashifmushtaq114@gmail.com>
2022-09-20 18:25:38 +0500

    Update CustomController.php

--- app/Http/Controllers/CustomController.php
+++ app/Http/Controllers/CustomController.php
@@ -7,6 +7,7 @@ use App\Models\Custom;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Exports\QuotesExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class CustomController extends Controller
 {
